66d9b488458b8f44b4cd0f4183e284a29abb7f90
Jack <qfhjack@gmail.com>
2019-08-16 22:37:06 -0700

    Create 118_Pascals_Triangle.py
    118. Pascals Triangle
    Category: Array
    Difficulty: Easy

---
+++ 118_Pascals_Triangle.py
@@ -0,0 +1,42 @@
+"""
+118. Pascals Triangle
+Category: Array
+Difficulty: Easy
+Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example:
+
+Input: 5
+Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+"""
+
+class Solution(object):
+    def generate(self, numRows):
+        result = []
+        for i in range(numRows):
+            result.append([])
+            for j in range(i+1):
+                if j in (0,i):
+                    result[i].append(1)
+                else:
+                    result[i].append(result[i-1][j-1] + result[i-1][j])
+        return result
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(Solution().generate(7))
